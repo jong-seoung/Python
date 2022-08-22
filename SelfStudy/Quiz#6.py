96c0c364ab0b72cd6f6a67f575a454160816eb50
DESKTOP-JBJR9JL\jongseoung <jjong015189@naver.com>
2022-08-22 16:23:03 +0900

    [Add] Quiz#6
    Quiz#6의 문제 정답 내용 추가

--- SelfStudy/Quiz#6.py
+++ SelfStudy/Quiz#6.py
@@ -15,6 +15,7 @@
 # 출력 예제
 # 키 175cm의 남자의 표준 체중은 67.38kg입니다.
 
+내가 푼 방법
 def std_weight(height,gender):
     if gender == "남자":
         print("키 {0}cm의 {1}의 표준 체중은 {2}kg입니다.".format(height,gender,round(height * height * 22 / 10000,2)))
@@ -24,3 +25,15 @@ def std_weight(height,gender):
     return height * height * 21
 
 std_weight(175,"남자")
+
+#정답
+def std_weight(height, gender):
+    if gender == "남자":
+        return height * height * 22
+    else:
+        return height * height * 21
+
+height = 175
+gender = "남자"
+weight = round(std_weight(height / 100 ,gender))
+print("키 {0}cm {1}의 표준체중은 {2}kg입니다.".format(height,gender,weight))

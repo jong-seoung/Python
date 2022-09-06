0a4dcd6266e5c268afdc21d4c4ef72dac690e0d3
DESKTOP-JBJR9JL\jongseoung <jjong015189@naver.com>
2022-09-06 13:10:34 +0900

    [Add] 1주차 수업 내용

---
+++ Python_ANYANG/1week_python.py
@@ -0,0 +1,15 @@
+print("Hello python")
+
+# 대화형 모드 : 터미널 창에 python / 우클릭 -> 대화형 창 
+# 종료 exit()
+
+print(2+5)
+print("2+5")
+
+print(3.14*10**2)
+
+print("l2"*10)
+
+print("나의 " + "고양이") # + , 문자열과 문자열 연결가능
+
+print("생일축하"*10)

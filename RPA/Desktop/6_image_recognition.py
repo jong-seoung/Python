b51898203c9eede226997ed989abd15c845e3425
DESKTOP-JBJR9JL\jongseoung <jjong015189@naver.com>
2022-10-19 16:33:07 +0900

    [데스크탑 UI 자동화] Title: 이미지 처리 -  대기

--- RPA/Desktop/6_image_recognition.py
+++ RPA/Desktop/6_image_recognition.py
@@ -28,5 +28,56 @@ import pyautogui
 # pyautogui.moveTo(trash_icon)
 
 # 3. 정확도 조정 ## pip install opencv-python 설치 필요
-run_btn = pyautogui.locateOnScreen("Desktop//run_btn.png", confidence=0.9) #90퍼센트이상 일치하면 작동
-pyautogui.moveTo(run_btn)
+# run_btn = pyautogui.locateOnScreen("Desktop//run_btn.png", confidence=0.9) #90퍼센트이상 일치하면 작동
+# pyautogui.moveTo(run_btn)
+
+### 자동화 대상이 바로 보여지지 않는 경우
+
+# 1. 계속 기다리기
+# notepad = pyautogui.locateOnScreen("Desktop//file_menu_notepad.png")
+# if notepad:
+#     pyautogui.click(notepad)
+# else:
+#     print("발견 실패")
+
+# while notepad is None:
+#     notepad = pyautogui.locateOnScreen("Desktop//file_menu_notepad.png")
+#     print("발견 실패")
+
+# pyautogui.click(notepad)
+
+# 2. 일정 시간동안 기다리기
+import time
+import sys
+
+# timeout = 10 # 10초 대기
+# start = time.time() # 시작 시간 설정 
+# notepad = None
+# while notepad is None:
+#     notepad = pyautogui.locateOnScreen("Desktop//file_menu_notepad.png")
+#     end = time.time() # 종료 시간 설정
+#     if end - start > timeout: # 지정한 10초를 초과하면
+#         print("시간 종료")
+#         sys.exit()
+
+# pyautogui.click(notepad)
+
+def find_target(img_file, timeout = 30):
+    start = time.time()
+    target=None
+    while target is None:
+        target = pyautogui.locateOnScreen(img_file)
+        end = time.time()
+        if end - start > timeout:
+            break
+    return target
+
+def my_click(img_file, timeout =30):
+    target = find_target(img_file,timeout)
+    if target:
+        pyautogui.click(target)
+    else:
+        print(f"[Timeout {timeout}s] Target not found({img_file}). Terminate program.")
+        sys.exit()
+
+my_click("Desktop//file_menu_notepad.png", 10)

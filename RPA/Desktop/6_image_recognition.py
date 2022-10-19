694b38c94f2add46da3496c495fd42dd275342a9
DESKTOP-JBJR9JL\jongseoung <jjong015189@naver.com>
2022-10-19 16:03:34 +0900

    [데스크탑 UI 자동화] Title: 이미지 처리 -  속도 개선

--- RPA/Desktop/6_image_recognition.py
+++ RPA/Desktop/6_image_recognition.py
@@ -4,8 +4,8 @@ import pyautogui
 # print(file_menu)
 # pyautogui.click(file_menu)
 
-trash_icon = pyautogui.locateOnScreen("Desktop//trash_icon.png")
-pyautogui.moveTo(trash_icon)
+# trash_icon = pyautogui.locateOnScreen("Desktop//trash_icon.png")
+# pyautogui.moveTo(trash_icon)
 
 # 화면 해상도, 이미지가 변경되면 오류가 날 가능성이 매우 높음
 
@@ -15,5 +15,18 @@ pyautogui.moveTo(trash_icon)
 #     pyautogui.click(i, duration=0.25)
 
 # 처음 나오는 이미지만 클릭하고 넘어가는 법
-check_box = pyautogui.locateOnScreen("Desktop//file_menu.png")
-pyautogui.click(check_box)
+# check_box = pyautogui.locateOnScreen("Desktop//file_menu.png")
+# pyautogui.click(check_box)
+
+### 속도 개선
+# 1. GrayScale - 흑백으로 만든 후 탐색 
+# trash_icon = pyautogui.locateOnScreen("Desktop//trash_icon.png", grayscale=True) # 정확도는 떨어질 수 있음
+# pyautogui.moveTo(trash_icon)
+
+# 2. 범위 지정
+# trash_icon = pyautogui.locateOnScreen("Desktop//trash_icon.png", region=x,y,width,height) # 탐색 시작 위치 x,y
+# pyautogui.moveTo(trash_icon)
+
+# 3. 정확도 조정 ## pip install opencv-python 설치 필요
+run_btn = pyautogui.locateOnScreen("Desktop//run_btn.png", confidence=0.9) #90퍼센트이상 일치하면 작동
+pyautogui.moveTo(run_btn)

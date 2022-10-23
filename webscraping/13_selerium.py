0d0702495a0932178ce81a8047a1983adba0f814
DESKTOP-JBJR9JL\jongseoung <jjong015189@naver.com>
2022-10-23 22:25:00 +0900

    [웹 스크래핑 : BeautifulSoup4] Title: Selenium 기본2

--- webscraping/13_selerium.py
+++ webscraping/13_selerium.py
@@ -14,3 +14,12 @@ elem = browser.find_element(By.ID,"query")
 from selenium.webdriver.common.keys import Keys
 elem.send_keys("구글")
 elem.send_keys(Keys.ENTER)
+
+elem = browser.find_elements(By.tag,"a") # 모든 정보 가지고오기 
+for e in elem:
+    e.get_attribute("href")
+
+elem = browser.find_element('xpath',"//*[@id='search_btn']/span[2]") # xpath로 설정
+elem.click() 
+browser.quit() # 브라우저 닫기
+exit()
